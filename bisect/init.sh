6657b521171672cc48dd1c868067ce84dba1a81c
brataTT <achoudhury@tenstorrent.com>
2025-09-16 14:18:02 -0400

    Update init script to use current repo instead of gist

--- bisect/init.sh
+++ bisect/init.sh
@@ -1,9 +1,11 @@
-mkdir up && \
-curl -L https://gist.github.com/6d17ff8f2a60ed883b9f08f394eb64cf/download -o gist.zip && \
+folders_to_move=(run map show)
+mkdir -p up && \
 tdir=$(mktemp -d) && \
-unzip gist.zip -d $tdir > /dev/null && \
-rm gist.zip && \
-mv $tdir/*/* up && \
-rm -r $tdir && \
-chmod +x up/* && \
+curl -L ${REPO_ZIP_URL:-https://github.com/jameszianxuTT/integration-tools/archive/refs/heads/main.zip} -o "$tdir/repo.zip" && \
+unzip "$tdir/repo.zip" -d "$tdir" > /dev/null && \
+rm "$tdir/repo.zip" && \
+srcdir=$(find "$tdir" -maxdepth 1 -type d -name "integration-tools-*" | head -n 1) && \
+for folder in "${folders_to_move[@]}"; do [ -d "$srcdir/$folder" ] && mv "$srcdir/$folder" up/; done && \
+rm -r "$srcdir" && \
+find up -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod +x {} + && \
 find up
